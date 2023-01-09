8c931cf416e2b12f8f30fa321e92e2badaf1cf6a
thaisohashi <ohashithais@gmail.com>
2023-01-08 22:12:28 -0300

    Adding exercises 83-94

---
+++ 01-Basic-JavaScript/92-manipulating-complex-objects.js
@@ -0,0 +1,18 @@
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Bring Me the Horizon",
+    title: "Sempiternal",
+    release_year: 2013,
+    formats: ["CD", "LP"],
+    gold: true,
+  },
+];
