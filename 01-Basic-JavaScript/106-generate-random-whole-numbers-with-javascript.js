76ccd6fd43118ede1c24e50e2adbd03f1559210b
thaisohashi <ohashithais@gmail.com>
2023-01-09 21:10:43 -0300

    Adding exercises 95-113

---
+++ 01-Basic-JavaScript/106-generate-random-whole-numbers-with-javascript.js
@@ -0,0 +1,6 @@
+// Use this technique to generate and return a random whole number between 0 and 9.
+
+function randomWholeNum() {
+  // Only change code below this line
+  return Math.floor(Math.random() * 10);
+}
